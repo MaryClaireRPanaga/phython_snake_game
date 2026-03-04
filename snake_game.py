9771327616983d2b3b2fb23bfdb993667cf6e9aa
MaryClaireRPanaga <maryclaireremigiopanaga@gmail.com>
2026-03-04 22:44:28 +0800

    initial commit

--- snake_game.py
+++ snake_game.py
@@ -2,7 +2,6 @@
 import turtle
 import random
 import time
-
 # creating turtle screen
 screen = turtle.Screen()
 screen.title('Snake Game')
@@ -52,29 +51,19 @@ scoring.penup()
 scoring.hideturtle()
 scoring.goto(0, 300)
 scoring.write("Score :", align="center", font=("Courier", 24, "bold"))
-
-
 #######define how to move
 def snake_go_up():
     if snake.direction != "down":
         snake.direction = "up"
-
-
 def snake_go_down():
     if snake.direction != "up":
         snake.direction = "down"
-
-
 def snake_go_left():
     if snake.direction != "right":
         snake.direction = "left"
-
-
 def snake_go_right():
     if snake.direction != "left":
         snake.direction = "right"
-
-
 def snake_move():
     if snake.direction == "up":
         y = snake.ycor()
@@ -88,8 +77,6 @@ def snake_move():
     if snake.direction == "right":
         x = snake.xcor()
         snake.setx(x + 20)
-
-
 # Keyboard bindings
 screen.listen()
 screen.onkeypress(snake_go_up, "Up")
@@ -108,7 +95,6 @@ while True:
         score += 1
         scoring.write("Score:{}".format(score), align="center", font=("Courier", 24, "bold"))
         delay -= 0.001
-
         ## creating new_ball
         new_fruit = turtle.Turtle()
         new_fruit.speed(0)
@@ -117,12 +103,10 @@ while True:
         new_fruit.penup()
         old_fruit.append(new_fruit)
     # adding ball to snake
-
     for index in range(len(old_fruit) - 1, 0, -1):
         a = old_fruit[index - 1].xcor()
         b = old_fruit[index - 1].ycor()
         old_fruit[index].goto(a, b)
-
     if len(old_fruit) > 0:
         a = snake.xcor()
         b = snake.ycor()
